9d4ae4d62e9150ba1e4fdc9d5975615566f75086
blingbling-110 <523497359@qq.com>
2022-05-18 12:34:03 +0800

    build: chunkLoadingGlobal

--- vue.config.js
+++ vue.config.js
@@ -12,7 +12,7 @@ module.exports = defineConfig({
     output: {
       library: `${name}-[name]`,
       libraryTarget: 'umd', // 把微应用打包成 umd 库格式
-      jsonpFunction: `webpackJsonp_${name}`
+      chunkLoadingGlobal: `webpackJsonp_${name}`
     }
   }
 })
